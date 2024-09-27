8656900f174e620468e057737ad95be545982257
Facundo Maldonado Marini <facumaldonadomarini@gmail.com>
2024-09-27 18:04:52 -0300

    Calculator day 27-09-24 v2

--- calculator/main.py
+++ calculator/main.py
@@ -2,61 +2,113 @@ import flet as ft
     
 async def main(page: ft.Page):
 
-    def buttons(text, bgndcolor, textcolor, btnheight, btnwidth):
-        return ft.TextButton(text=text,
+    global total_operation
+    total_operation = ""
+
+    global current_number
+    current_number = 0
+
+    global numbers
+    numbers = []
+
+    global operations
+    operations = []
+
+    def buttons(value, bgndcolor, textcolor, btnheight, btnwidth):
+        return ft.TextButton(text=value,
                 height=btnheight,
                 width=btnwidth,
-                style=ft.ButtonStyle(bgcolor=bgndcolor, color=textcolor))
+                style=ft.ButtonStyle(bgcolor=bgndcolor, color=textcolor),
+                data=value,
+                on_click=keyPress)
+
     
     page.bgcolor = ft.colors.BLACK
     page.vertical_alignment = ft.CrossAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
-    page.window_height = 600
+    page.window_height = 500
     page.window_width = 400
     page.title = "Calculator"
     page.window_center()
 
-    def keyPress(value):
-        number = f"{number}{value}"
+    def keyPress(a):
 
-    number = ""
+        value = a.control.data
+        try:
+            int(value)
+            global total_operation
+            total_operation = f"{total_operation}{value}"
+            screen.value = total_operation
+        except:
+            if total_operation[-1] == "+" or total_operation[-1] == "-" or total_operation[-1] == "*" or total_operation[-1] == "/":
+                return 0
 
-    keys = ft.GridView( runs_count=4,
-                        height=page.window_height,
-                        width=page.window_width,
-                        child_aspect_ratio=1.8,
-                        bottom=0)
+            total_operation = f"{total_operation}{value}"
+            
+            if value == "+":
+                operations.append("+")
+
+            elif value == "-":
+                operations.append("-")
+
+            elif value == "*":
+                operations.append("*")
+
+            elif value == "/":
+                operations.append("/")
+
+                print(operations)
+
+            else:
+                do_operation(operation)
+        
+        page.update()
+
+    keys = ft.GridView(
+        runs_count=4,
+        height=page.window.height*60/100,
+        width=page.window_width,
+        child_aspect_ratio=1.8,
+    )
     
-    screen = ft.TextField(read_only=True, value=number)
+    screen = ft.TextField(read_only=True,
+                          value=total_operation,
+                          height=page.window.height*20/100,
+                          width=page.window_width,
+                          color=ft.colors.WHITE)
     
-    mainStack = ft.Stack([ft.Container(bgcolor=ft.colors.AMBER), keys, screen],
-                        height=page.window_height,
-                        width=page.window_width,
-                        alignment=ft.alignment.center,
-                        )
+    mainColumn = ft.Column(
+        [
+            screen,
+            ft.Container(keys, alignment=ft.alignment.bottom_center),
+        ],
+        alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+        height=page.window_height,
+        width=page.window_width
+    )
     
     keys.controls.append(buttons("1",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("2",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("3",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
-    keys.controls.append(buttons("+",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("+",ft.colors.AMBER_ACCENT_700, ft.colors.BLACK, keys.height/7, keys.width/4.5))
 
     keys.controls.append(buttons("4",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("5",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("6",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
-    keys.controls.append(buttons("-",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("-",ft.colors.AMBER_ACCENT_700, ft.colors.BLACK, keys.height/7, keys.width/4.5))
 
     keys.controls.append(buttons("7",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("8",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("9",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
-    keys.controls.append(buttons("*",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("*",ft.colors.AMBER_ACCENT_700, ft.colors.BLACK, keys.height/7, keys.width/4.5))
 
-    keys.controls.append(buttons("AC",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("AC",ft.colors.RED_400, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     keys.controls.append(buttons("0",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
-    keys.controls.append(buttons("=",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
-    keys.controls.append(buttons("/",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("=",ft.colors.BLUE_ACCENT_700, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("/",ft.colors.AMBER_ACCENT_700, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     
 
-    page.add(mainStack)
+    page.add(mainColumn)
 
 
 ft.app(main)
