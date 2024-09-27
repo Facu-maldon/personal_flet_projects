39780972b1c860f2a30789013abecd33268c3ef0
Facundo Maldonado Marini <facumaldonadomarini@gmail.com>
2024-09-27 11:49:22 -0300

    calculator day 27-09-24 1

--- calculator/main.py
+++ calculator/main.py
@@ -11,59 +11,49 @@ async def main(page: ft.Page):
     page.bgcolor = ft.colors.BLACK
     page.vertical_alignment = ft.CrossAxisAlignment.CENTER
     page.horizontal_alignment = ft.MainAxisAlignment.CENTER
-    page.window.height = 800
-    page.window.width = 700
+    page.window_height = 600
+    page.window_width = 400
     page.title = "Calculator"
+    page.window_center()
 
-    mainStack = ft.Stack([ft.Container(bgcolor=ft.colors.AMBER)],
-                         height=page.window.height,
-                         width=page.window.width,
-                         alignment=ft.alignment.center)
+    def keyPress(value):
+        number = f"{number}{value}"
 
-    keys = ft.ResponsiveRow(height=mainStack.height*60/100,
-                            width=mainStack.width,
-                            bottom=0,
-                            left=0)
+    number = ""
+
+    keys = ft.GridView( runs_count=4,
+                        height=page.window_height,
+                        width=page.window_width,
+                        child_aspect_ratio=1.8,
+                        bottom=0)
     
-    mainStack.controls.append(keys)
+    screen = ft.TextField(read_only=True, value=number)
     
-    keys.controls.append(
-        ft.Row([
-            buttons("1",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("2",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("3",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("+",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-                ],
-            width=keys.width,
-            alignment=ft.MainAxisAlignment.CENTER
-        ))
+    mainStack = ft.Stack([ft.Container(bgcolor=ft.colors.AMBER), keys, screen],
+                        height=page.window_height,
+                        width=page.window_width,
+                        alignment=ft.alignment.center,
+                        )
+    
+    keys.controls.append(buttons("1",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("2",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("3",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("+",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
 
-    keys.controls.append(
-        ft.Row([
-            buttons("4",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("5",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("6",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("-",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-                ],
-                )
-        )
+    keys.controls.append(buttons("4",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("5",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("6",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("-",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
 
-    keys.controls.append(
-        ft.Row([
-            buttons("7",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("8",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("9",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("*",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-                ], 
-            ))
+    keys.controls.append(buttons("7",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("8",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("9",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("*",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
 
-    keys.controls.append(
-        ft.Row([
-            buttons("AC",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("0",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("=",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-            buttons("/",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5),
-                ]))
+    keys.controls.append(buttons("AC",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("0",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("=",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
+    keys.controls.append(buttons("/",ft.colors.WHITE, ft.colors.BLACK, keys.height/7, keys.width/4.5))
     
 
     page.add(mainStack)
