099ce8daa6e32536d3bfd40d64aeb6422c1853c8
Facundo Maldonado Marini <facumaldonadomarini@gmail.com>
2024-10-03 19:16:57 -0300

    formatter day 03-10-24 v1

--- csv/main.py
+++ csv/main.py
@@ -1,12 +1,40 @@
 import flet as ft
-
-import pandas as pd
-
-
-
+from screeninfo import get_monitors
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
 
+    def mostrarArchivo(e: ft.FilePickerResultEvent):
+        print(nombreArchivo)
+        nombreArchivo.helper_text = e.files
+        rutaArchivo.helper_text = e.path
+        page.update()
+
+
+    monitores = get_monitors()
+
+    page.window.width = monitores[0].width/5
+    page.window.height = monitores[0].height/2
+
+    page.title = "Formateador Excel a TXT"
+
+    selector = ft.FilePicker(on_result=mostrarArchivo)
+
+    page.overlay.append(selector)
+
+    global nombreArchivo
+
+    global rutaArchivo
+
+    nombreArchivo = ft.TextField(label="Nombre del archivo", disabled=True)
+
+    rutaArchivo = ft.TextField(label="Ruta del archivo", disabled=True)
+
+    columna = ft.ResponsiveRow([ft.ElevatedButton(on_click=lambda _: selector.pick_files(), text="Elegir archivo"),
+                                nombreArchivo, rutaArchivo],
+                                height=page.window.height, width=page.window.width)
+
+
+
+    page.add(columna)
 
 ft.app(main)
