774632134d3254498a3cb62e2263cc34021661b6
Facundo Maldonado Marini <facumaldonadomarini@gmail.com>
2024-10-03 19:16:23 -0300

    reader 03-10-24 (finished)

--- csv/reader.py
+++ csv/reader.py
@@ -1,44 +1,47 @@
 import pandas as pd
 
-to_read = "C:/Users/Usuario2/Desktop/Pasantias/test_csv.xlsx"
 
-file = pd.read_excel(to_read)
+def formatearExcelATxt(archivoOrigen):
+    dataFrame = pd.read_excel(archivoOrigen)
+    dataFrame = dataFrame.head(len(dataFrame))
 
-file = file.head(len(file))
+    datos = [i for i in dataFrame]
 
-datos = [i for i in file]
+    longitudes = [4, 10, 3, 6, 4, 9, 13, 30, 2, 8]
 
-longitudes = [4, 10, 3, 6, 4, 9, 13, 30, 2, 8]
+    if len(datos) != len(longitudes):
+        print("Error en la carga de datos")
+        return 2
 
-if len(datos) != len(longitudes):
-    print("Error en la carga de datos")
-
-for i in range(0, len(file)):
-    
-    contador_longitudes = 0
-    linea = ""
-    for dato in datos:
-
-        dato = file[dato][i]
-        long = len(str(dato))
-        faltantes = longitudes[contador_longitudes]-long
-
-        try:
-            dato = int(dato)
-
-            if len(str(dato)) < longitudes[contador_longitudes]:
-                dato = int(f"{dato}{"0"*faltantes}")
-
-        except ValueError:
-            dato = str(dato)
-            if len(dato) < longitudes[contador_longitudes]:
-                dato = f"{dato}{" "*faltantes}"
-
-        linea = f"{linea}{dato}"
-        print(dato, len(str(dato)))
-        contador_longitudes += 1
+    for i in range(0, len(dataFrame)):
         
+        contador_longitudes = 0
+        linea = ""
+        if len(dataFrame["Numero de documento"][i]) != 8:
+            print(f"Número de documento de la columna {i} incorrecto(escribirlo sin puntos y revisar error en la escritura)")
+            return 1
+        
+        for dato in datos:
 
-    linea = f"{linea}\n"
-    print(linea, len(linea), linea[0])
+            dato = dataFrame[dato][i]
+            long = len(str(dato))
+            faltantes = longitudes[contador_longitudes]-long
+
+            try:
+                dato = int(dato)
+
+                if len(str(dato)) < longitudes[contador_longitudes]:
+                    dato = int(f"{dato}{"0"*faltantes}")
+
+            except ValueError:
+                dato = str(dato)
+                if len(dato) < longitudes[contador_longitudes]:
+                    dato = f"{dato}{" "*faltantes}"
+
+            linea = f"{linea}{dato}"
+            contador_longitudes += 1
+            
+
+        linea = f"{linea}\n"
+
 
