619349eb3c00a65bb54142c8fa33eb1be42d1378
Facundo Maldonado Marini <facumaldonadomarini@gmail.com>
2024-10-04 17:32:37 -0300

    formatter 04-10-24 stable(?)

--- csv/main.py
+++ csv/main.py
@@ -1,19 +1,27 @@
 import flet as ft
+import pandas as pd
+from reader import formatearExcelATxt
 from screeninfo import get_monitors
 
 def main(page: ft.Page):
-
     def mostrarArchivo(e: ft.FilePickerResultEvent):
-        print(nombreArchivo)
-        nombreArchivo.helper_text = e.files
-        rutaArchivo.helper_text = e.path
+        archivo = e.files[0]
+
+        columna.controls.pop()
+        columna.controls.pop()
+
+        columna.controls.append(ft.Text(f"Nombre del archivo:\n {archivo.name}"))
+        columna.controls.append(ft.Text(f"Ruta del archivo:\n {archivo.path.replace("\\", "/")}"))
+
+        botonConvertir.data = archivo.path.replace("\\", "/")
+        botonConvertir.on_click = formatearExcelATxt
+
         page.update()
+        
+    pantallas = get_monitors()
 
-
-    monitores = get_monitors()
-
-    page.window.width = monitores[0].width/5
-    page.window.height = monitores[0].height/2
+    page.window.width = pantallas[0].width/5
+    page.window.height = pantallas[0].height/2
 
     page.title = "Formateador Excel a TXT"
 
@@ -21,20 +29,22 @@ def main(page: ft.Page):
 
     page.overlay.append(selector)
 
-    global nombreArchivo
+    columna = ft.ResponsiveRow([ft.ElevatedButton(
+                                    on_click=lambda _: selector.pick_files(file_type=ft.FilePickerFileType.CUSTOM,
+                                    allowed_extensions = ["xls", "xlsx"],
+                                    allow_multiple = False), 
+                                    text="Elegir archivo"),
+                                ft.Text("Archivo no seleccionado"),
+                                ft.Text()],
+                                height=page.window.height*75/100, width=page.window.width)
+    
+    global botonConvertir
+    botonConvertir = ft.ElevatedButton(text="Convertir")
 
-    global rutaArchivo
+    columnaBoton = ft.ResponsiveRow([botonConvertir],height=page.window.height*25/100, width=page.window.width)
 
-    nombreArchivo = ft.TextField(label="Nombre del archivo", disabled=True)
+    page.controls.append(columna)
 
-    rutaArchivo = ft.TextField(label="Ruta del archivo", disabled=True)
-
-    columna = ft.ResponsiveRow([ft.ElevatedButton(on_click=lambda _: selector.pick_files(), text="Elegir archivo"),
-                                nombreArchivo, rutaArchivo],
-                                height=page.window.height, width=page.window.width)
-
-
-
-    page.add(columna)
+    page.add(columnaBoton)
 
 ft.app(main)
